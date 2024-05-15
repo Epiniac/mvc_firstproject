323dddfcb504892038bf4eb67e56e80def49938d
cognitech <j_chen39@hetic.eu>
2024-05-15 17:22:37 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# mvc_firstproject
-
-
-https://www.blackbox.ai/share/1095c438-5667-484b-988a-6ffa67f2da3e
+# MVC Models for a chemistry lab
--- index.php
+++ index.php
@@ -7,22 +7,27 @@ $moleculeModel = new Molecule($db);
 $moleculeController = new Controller($moleculeModel);
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'viewMolecules';
-switch ($action) {
-  case 'viewMolecules':
-    $moleculeController->viewMolecules();
-    break;
-  case 'viewMolecule':
-    $moleculeController->viewMolecule();
-    break;
-  case 'createMolecule':
-    $moleculeController->createMolecule();
-    break;
-  case 'updateMolecule':
-    $moleculeController->updateMolecule();
-    break;
-  case 'deleteMolecule':
-    $moleculeController->deleteMolecule();
-    break;
-  default:
-    header('Location: ?action=viewMolecules');
+try{
+  switch ($action) {
+    case 'viewMolecules':
+      $moleculeController->viewMolecules();
+      break;
+    case 'viewMolecule':
+      $moleculeController->viewMolecule();
+      break;
+    case 'createMolecule':
+      $moleculeController->createMolecule();
+      break;
+    case 'updateMolecule':
+      $moleculeController->updateMolecule();
+      break;
+    case 'deleteMolecule':
+      $moleculeController->deleteMolecule();
+      break;
+    default:
+      header('Location: ?action=viewMolecules');
+  }
+} catch (Exception $e) {
+  include __DIR__ . '/views/errors/500.php';
+  throw new Exception("Error Processing Request: " + $e, 1);
 }
